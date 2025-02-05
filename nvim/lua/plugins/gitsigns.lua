5a933cee30df8c38ebf4fbe296594c299739e9ef
MistrAnderson <noteo.gaming@protonmail.com>
2025-02-05 01:11:47 +0100

    fix (Neovim): Gitsigns now loads on every buffer, this fixes the fact that it was not loading if calling nvim from outside a git repo

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,10 +1,10 @@
 return {
   "lewis6991/gitsigns.nvim",
-  cond = function()
-    -- Check if the current buffer is within a Git repository
-    local is_git_repo = vim.fn.systemlist("git rev-parse --is-inside-work-tree")[1] == "true"
-    return is_git_repo
-  end,
+  -- cond = function()
+  --   -- Check if the current buffer is within a Git repository
+  --   local is_git_repo = vim.fn.systemlist("git rev-parse --is-inside-work-tree")[1] == "true"
+  --   return is_git_repo
+  -- end,
   config = function()
     require("gitsigns").setup()
   end,
