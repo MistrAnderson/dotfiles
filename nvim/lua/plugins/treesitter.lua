86ff0fc9d1f0259280d00f29171286cd647eb8a1
MistrAnderson <noteo.gaming@protonmail.com>
2025-02-27 21:19:09 +0100

    feat (nvim): add two plugins for autocompletion of tsx tags and for TODO management

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
-      ensure_installed = { "lua", "python", "php", "markdown" },
+      ensure_installed = { "lua", "python", "markdown" },
       highlight = { enable = true },
       indent = { enable = true },
     })
