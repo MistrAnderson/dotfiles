49943bcf22cfb3545b0cb7c92bcd15df2bf4f48a
MistrAnderson <noteo.gaming@protonmail.com>
2025-03-25 19:30:16 +0100

    feat (yazi): add yazi alias and editor config

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,15 @@
 alias tmux='tmux -2'
 alias ll='ls -la'
 alias ls='ls --color=always'
+alias v='nvim'
 alias vi='nvim'
 # alias bat='batcat'
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
